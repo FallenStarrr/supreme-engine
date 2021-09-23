a52f594bac99d11d2d3f2756c53e6f2b2e6777d2
Vlad <juzopigeon@gmail.com>
2021-09-23 13:48:34 +0600

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import './App.css';
+
+function App() {
+  const [drag, setDrag] = useState(false)
+
+  function dragStartHandler(e) {
+    e.preventDefault()
+    setDrag(true)
+  }
+
+
+  function dragLeaveHandler(e) {
+    e.preventDefault()
+    setDrag(false)
+  }
+
+  function  onDropHandler(e) {
+      e.preventDefault()
+      let files = [...e.dataTransfer.files]
+      const formData = new FormData()
+      formData.append('file', files[0])
+      formData.append('userId', 1)
+      // axios.post('url', formData, options)
+      console.log(files)
+      setDrag(false)
+  }
+  return (
+    <div className="app">
+      {
+       drag
+       ? <div className="drop-area"
+        onDragStart={e => dragStartHandler(e)}
+         onDragLeave={e => dragLeaveHandler(e)}
+         onDragOver={e => dragStartHandler(e)}
+         onDrop={e => onDropHandler(e)}
+       >Отпустите файлы чтобы загрузить их</div>
+       : <div
+         onDragStart={e => dragStartHandler(e)}
+         onDragLeave={e => dragLeaveHandler(e)}
+         onDragOver={e => dragStartHandler(e)}
+       >Перетащите файлы чтобы загрузить их</div>
+      }
+    </div>
+    
+  );
+}
+
+export default App;
